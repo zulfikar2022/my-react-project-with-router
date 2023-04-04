a53d827a08ba6a9a74c170e74a8603c1feab81dd
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-04-04 21:20:54 +0600

    conctact page created but it seems critical to understand

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import Contacts from './routes/contacts';
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>
+  },
+  {
+      path:'contacts/:contactId',
+      element:<Contacts></Contacts>
+
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
